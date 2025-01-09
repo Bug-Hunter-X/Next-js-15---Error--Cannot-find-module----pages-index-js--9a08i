a026b3b975f8f7c86c90b4fa967d88575980223b
BugHunter-X <askzenai@gmail.com>
2025-01-09 13:28:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,10 @@
+```javascript
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div>Hello world!</div>
+  );
+}
+```
+This code works fine in Next.js 14 but throws an error in Next.js 15: `Error: Cannot find module './pages/index.js'
